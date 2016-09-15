2d300196f780fcf8a4ae7ace840cdd5a5613464d
anniekbh <anniekbh@gmail.com>
2016-09-15 12:11:07 -0500

    subiendo cambios

---
+++ README.md
@@ -0,0 +1,12 @@
+# BICICLETAS LOCAS 
+El codigo JS de este proyecto, fue creado para validacion de datos ingresandos a un formulario simple.
+![bicis](http://i65.tinypic.com/24l4t9h.png)
+1. Validar que las primeras 4 areas sean obligatorias
+2. Validar que los campos Nombre y Apellido, se permitan solo caracteres de A-Z
+3. Validar que los campos Nombre y Apellido, se inicien con una letra mayuscula
+4. Validar que el campo e-mail tenga un formato valido
+5. Validar que el campo password debe tener al menos 6 caracteres
+6. validar que el campo password no sea igual a password, 123456 o 098754
+7. El valor seleccionado de bicis, debe ser una de las opciones presentadas
+8. Si algun campo presenta error debe informarse al usuario por medio de un alert.
+9. fin.
--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var x = document.getElementById("name").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+    var x = document.getElementById("lastname").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+    var x = document.getElementById("input-email").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+    var x = document.getElementById("input-password").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+   	var nom = document.getElementById("input-password").value;
+    if ( nom == "123456" || nom == "098754" || nom == "password" || nom.length < 5 || /^\s+$/.test(nom)) {
+      window.alert ("No Válido") ;
+    } else {
+        return true;
+    };
+    var email = document.getElementById("input-email").value;
+	if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
+  	return false;
+	};
+	if(document.getElementsByTagName('select')[0].value == 0){
+            alert("Selecciona un tipo de bici");
+    };
 }
